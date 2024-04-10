2c2db1213ba20f0be013c32c5cc6fb2b2df2d012
ArbazKhan-Developer <khan.shahbaz4595@gmail.com>
2024-04-10 15:32:16 +0530

    port added

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.use(express.urlencoded({extended: true}));
 
 
 app.get("/", (req, res) => {
+    console.log('request received');
   res.writeHead(200, {
     "Content-Type": "text/plain",
   });
